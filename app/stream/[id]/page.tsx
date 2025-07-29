459eb3a756faa4816e712a3e5f735d885f20e9a0
Hashir-Khan777 <khnhashir555@gmail.com>
2025-07-30 01:02:04 +0500

    fieldhouse staging

--- app/stream/[id]/page.tsx
+++ app/stream/[id]/page.tsx
@@ -145,11 +145,16 @@ export default function StreamPage() {
         iceServers: [
           { urls: "stun:stun.l.google.com:19302" },
           {
-            urls: "turn:openrelay.metered.ca:80",
+            urls: [
+              "turn:openrelay.metered.ca:80",
+              "turn:openrelay.metered.ca:443",
+              "turn:openrelay.metered.ca:443?transport=tcp",
+            ],
             username: "openrelayproject",
             credential: "openrelayproject",
           },
         ],
+        iceTransportPolicy: "relay",
       });
       peersRef.current[id] = pc;
 
@@ -198,11 +203,16 @@ export default function StreamPage() {
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
         {
-          urls: "turn:openrelay.metered.ca:80",
+          urls: [
+            "turn:openrelay.metered.ca:80",
+            "turn:openrelay.metered.ca:443",
+            "turn:openrelay.metered.ca:443?transport=tcp",
+          ],
           username: "openrelayproject",
           credential: "openrelayproject",
         },
       ],
+      iceTransportPolicy: "relay",
     });
     peersRef.current["broadcaster"] = pc;
 
